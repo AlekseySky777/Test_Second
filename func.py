920da5222579faebfc6d1e5186ec440931444f8e
AlekseySky777 <aleksey_schelkunov@hotmail.com>
2024-06-08 23:34:37 +0100

    Add files via upload

---
+++ func.py
@@ -0,0 +1,7 @@
+def my_func():
+    print("Hello!")
+    
+    
+def my_func_2():
+    print("Good morning!")
+
---
+++ main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import func
+import math as mt
+
+#print("Hello!")
+func.my_func()
+print("Factorial from 5:", mt.factorial(5))
+
